282cfec5ed70616ba7d611a4cabf5fddce452363
ag <me@ag.com>
2023-09-25 20:43:25 +0500

    update

--- nvim/lua/ag/plugins/colorschemear.lua
+++ nvim/lua/ag/plugins/colorschemear.lua
@@ -36,27 +36,27 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		lazy = false,
-		-- config = function()
-		--   vim.cmd.colorscheme("catppuccin-mocha")
-		-- end,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
 	},
 	{
 		"projekt0n/github-nvim-theme",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("github-theme").setup({})
-			vim.cmd("colorscheme github_dark_high_contrast")
-		end,
+		-- config = function()
+		-- 	require("github-theme").setup({})
+		-- vim.cmd("colorscheme github_dark_high_default")
+		-- end,
 	},
 
 	{
 		"EdenEast/nightfox.nvim",
 		lazy = false,
 		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("github-theme").setup({})
-			-- vim.cmd("colorscheme nightfox")
-		end,
+		-- config = function()
+		-- 	require("github-theme").setup({})
+		-- 	vim.cmd("colorscheme nightfox")
+		-- end,
 	},
 }
