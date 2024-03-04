f445f6960c7303b9c762516844ad834c559a2482
ahsanghalib <ahsanghalib@gmail.com>
2024-03-05 02:28:07 +0500

    new config

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "3024 Night"
+config.color_scheme = "Ayu Dark (Gogh)"
 config.font_size = 14
 config.font = wezterm.font("JetBrains Mono")
 
